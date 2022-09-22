950771be9f7cd5b3c6821df65991ed2d2a5d5f6e
Calvert <dyverny@gmail.com>
2022-09-22 07:10:36 +0000

    Print lowercase alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Print alphabets in lower case
+ *
+ * Return : Always 0
+ */
+void main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int b;
+
+	for (a = 0; a < 26; a++)
+	{
+		print_alphabet(alph[b]);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,3 @@
 # 0x02. C - Functions, nested loops
-1. Print _putchar, followed by a new line.
-2. 
+1. Print '_putchar'
+2. Print alphabets in lowercase
