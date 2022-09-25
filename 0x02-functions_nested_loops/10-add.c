c36692c5844a3175dcf3cc9e54f09d9f6e4c4fd0
Calvert <dyverny@gmail.com>
2022-09-25 14:25:07 +0000

    Followed Betty coding style

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,13 +3,17 @@
 /**
  * add - Return the sum of two integers
  *
- * Return - 0
+ * @a: Input any integer
+ * @b: Input any integer
+ *
+ * Return: 0
  */
 int add(int a, int b)
 {
 	int c;
+
 	c = a + b;
 	_putchar(c);
 	_putchar('\n');
-	return(0);
+	return (0);
 }
