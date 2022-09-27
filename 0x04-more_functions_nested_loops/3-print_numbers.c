081053bc8d92836780dc66d32b7643393c9e9474
Calvert <dyverny@gmail.com>
2022-09-27 08:43:38 +0000

    Code works now

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,13 @@
 /**
  * print_numbers - Print numbers from 0 to 9
  *
- * Return: Always 0
+ * Return: No return
  */
 void print_numbers(void)
 {
 	int a;
 
-	for (a = 0; a < 58; a++)
+	for (a = 48; a < 58; a++)
 	{
 		_putchar(a);
 	}
