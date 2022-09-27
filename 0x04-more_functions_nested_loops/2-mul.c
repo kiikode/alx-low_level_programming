75b3156ae15980d6ecd5f36290b39b28b0895354
Calvert <dyverny@gmail.com>
2022-09-27 08:23:05 +0000

    Find the product

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -10,6 +10,20 @@
  */
 int mul(int a, int b)
 {
-	_putchar(a * b);
+	int pr;
+
+	pr = a * b;
+	if ((a < 0 && b > 0) || (a > 0 && b < 0))
+	{
+		_putchar(pr);
+	}
+	else if (a < 0 && b < 0)
+	{
+		_putchar(-pr);
+	}
+	else
+	{
+		_putchar(pr);
+	}
 	_putchar('\n');
 }
