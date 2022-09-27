c7936acfcf879619fdc67e506e7e37eba44cdb83
Calvert <dyverny@gmail.com>
2022-09-27 07:46:48 +0000

    Few changes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,13 +9,16 @@
  */
 int _isupper(int c)
 {
-	if (c >= 65; c <= 90)
+	for (c = 0; c++)
 	{
-		return(1);
+		if (c >= 65; c <= 90;)
+		{
+			return(1);
+		}
+		else
+		{
+			return(0);
+		}
+		_putchar('\n');
 	}
-	else
-	{
-		return(0);
-	}
-	_putchar('\n');
 }
