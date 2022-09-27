342e25b8b6add8b97d8d7293c9db5860289db3f3
Calvert <dyverny@gmail.com>
2022-09-27 08:00:58 +0000

    Removed a statement having no effect

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,16 +9,13 @@
  */
 int _isupper(int c)
 {
-	for (c = 0; c++)
+	if (c >= 65 && c <= 90)
 	{
-		if (c > 64; c < 91)
-		{
-			return(1);
-		}
-		else
-		{
-			return(0);
-		}
-		_putchar('\n');
+		return (1);
 	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
 }
