c6f5cae39dc6ccc79759d75f889f774ee0571439
Calvert <dyverny@gmail.com>
2022-09-29 08:25:37 +0000

    Checked for correct name of variable used

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	int num;
-
+	
 	for (num = 1; num <= 100; num++)
 	{
-		if ((num % 3 == 0) && (n % 5 == 0))
+		if ((num % 3 == 0) && (num % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		}
