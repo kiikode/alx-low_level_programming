298207d04386c6eb2bc05ac8306f34978d4c2971
Calvert <dyverny@gmail.com>
2022-09-29 08:41:12 +0000

    Used the correct parameter

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,7 +13,7 @@ void print_triangle(int size)
 
 	for (a = 0; a < size; a++)
 	{
-		for (b = 1; b < (size - 1); b++)
+		for (b = 1; b < (size - a); b++)
 			_putchar(' ');
 		for (b--; b < size; b++)
 			_putchar(35);
