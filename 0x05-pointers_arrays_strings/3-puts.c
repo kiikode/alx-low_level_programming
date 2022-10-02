0fb68545d2a9e54af7b8db78f3cb34d8aa6e9ad5
Calvert <dyverny@gmail.com>
2022-10-02 18:11:28 +0000

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Print a string to standard output
+ *
+ * @str: Pointer to a character
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	char string;
+
+	*str = string;
+	_putchar(*str);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,4 +1,5 @@
 #0x05. c-Pointers, arrays and strings
 1. Take a pointer to an int as a parameter
 2. Swap the values of two integers
-3. Return the length of a string 
+3. Return the length of a string
+4. Print a string to stdout 
