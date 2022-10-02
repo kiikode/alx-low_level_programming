cd01364d1057bf989108b03b9f54915106ad0daa
Calvert <dyverny@gmail.com>
2022-10-02 15:52:02 +0000

    The use of a pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
-#include "alx.h"
+#include "main.h"
 
 /**
  * reset_to_98 - takes a pointer to a parameter and updates its value.
  *
- * Return: nothing
+ * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
