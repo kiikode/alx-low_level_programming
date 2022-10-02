7022db5512ea4fa3c71db67ce25af69603334cb9
Calvert <dyverny@gmail.com>
2022-10-02 18:28:57 +0000

    Print a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Print a string in reverse
+ *
+ * @s: Pointer to a character
+ *
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -2,4 +2,5 @@
 1. Take a pointer to an int as a parameter
 2. Swap the values of two integers
 3. Return the length of a string
-4. Print a string to stdout 
+4. Print a string to stdout
+5. Print a string in reverse 
