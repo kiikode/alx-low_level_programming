19e0fd7a5fbc699a67214c8d64d96652093ab3e2
Calvert <dyverny@gmail.com>
2022-10-08 15:19:52 +0000

    Exponentiation

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Return the value of x raised to
+ * the power of y
+ *
+ * @x: The base
+ * @y: The exponent
+ *
+ * Return: Success
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -3,3 +3,4 @@
 2. A function that prints a string in reverse
 3. A function that returns the length of a string
 4. A function that returns the factorial of a given number
+5. A function that returns the value of x raised to the power of y
