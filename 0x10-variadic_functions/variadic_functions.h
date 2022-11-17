b2b71d41b03eae0f734cc048df05c25fdbced2cd
Calvert <dyverny@gmail.com>
2022-11-17 14:30:28 +0000

    Return the sum of all its parameters

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,8 +6,5 @@
 
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
-void print_numbers(const *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
 
 #endif
