403e12a5c21db3568029d695e71552e29499a0ed
Calvert <dyverny@gmail.com>
2022-11-17 15:32:30 +0000

    Print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,43 +14,43 @@ void print_all(const char * const format, ...)
 	char *str;
 	const char t_arg[] = "cifs";
 	va_start(valist, format);
-
-		while (format && format[i])
+	
+	while (format && format[i])
+	{
+		j = 0;
+		while(t_arg[j])
 		{
-			j = 0;
-			while(t_arg[j])
+			if (format[i] == t_arg[j] && c)
 			{
-				if (format[i] == t_arg[j] && c)
-				{
-					printf(", ");
-					break;
-				}
-				j++;
-			}
-			switch (format[i])
-			{
-			case 'c':
-				printf("%c", va_arg(valist, char)), c = 1;
-				break;
-			case 'i':
-				printf("%d", va_arg(valist, int)), c = 1;
-				break;
-			case 'f':
-				printf("%f", va_arg(valist, double)), c = 1;
-				break;
-			case 's':
-				str = va_grant(valist, char *), c = 1;
-				if (str == NULL)
-				{
-					printf("(nil)");
-					break;
-				}
-				printf("%s", str);
+				printf(", ");
 				break;
 			}
-			i++;
+			j++;
 		}
-		printf("\n");
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, char)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_grant(valist, char *), c = 1;
+			if (str == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		}
+		i++;
+	}
+	printf("\n");
 
-		va_end(valist);
+	va_end(valist);
 }
