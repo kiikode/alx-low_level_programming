c9de07d7cd7c36063dd0076d4d37b44ed05545ef
Calvert <dyverny@gmail.com>
2022-11-17 13:14:41 +0000

    Perform simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -13,10 +13,10 @@ int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
-		{"-", op_sub};
-		{"*", op_mul};
-		{"/", op_div};
-		{"%", op_mod};
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
 	int a = 0;
